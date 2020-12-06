a2fe66f6b47b8802a47cf3cb2113900b7a5a14be
Domattee <domattee@yahoo.de>
2020-12-06 16:41:54 +0100

    Comments

--- generate.py
+++ generate.py
@@ -1,5 +1,3 @@
-''' Sample
-   This script loads a pretrained net and a weightsfile and sample '''
 import numpy as np
 import os
 
@@ -10,8 +8,21 @@ import utils
 import BigGAN
 import matplotlib.pyplot as plt
 
-#	dir should point to the directory containing the G or G_ema weights.
-def load(dir):
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+"""
+Loads a pretrained model from dir.
+
+Args:
+	dir:	String containing the path to the directory with the .pth files
+	model:	The specific .pth file that should be loaded. Must match the BigGan generator.
+	
+Returns:
+	BigGan Generator object
+
+"""
+def load(dir, model):
+	# Parameters which were used for the pretrained model
 	strict = True
 	params = {}
 	params["G_ch"] = 96
@@ -23,41 +34,64 @@ def load(dir):
 	params["no_optim"] = True
 	params["G_activation"] = utils.activation_dict["inplace_relu"]
 	
-	G = BigGAN.Generator(**params).cuda()
-	G.load_state_dict(torch.load(os.path.join(dir, "G_ema.pth")), strict=strict)
-	#G.optim.load_state_dict(torch.load(os.path.join(dir, "G_optim.pth")), strict=strict)
-	#state_dict = torch.load(os.path.join(dir, "state_dict.pth"))
+	# Use GPU if possible
+	if device == "cuda":
+		G = BigGAN.Generator(**params).cuda()
+	else:
+		G = BigGAN.Generator(**params).cpu()
+		
+	# Load the weights
+	G.load_state_dict(torch.load(os.path.join(dir, model)), strict=strict)
 	return G
 
 	
-	# Required a Generator object G. 
-	#	im_class is an integer representing the imagenet class.
-	#	n_images is the number of images to generate
-	#	n_columns determines how many images will be saved in a single row
-	#	filename is the path where the image will be stored, if None the image isn't saved.
+"""
+Generates a number of images of a given class and displays them in a grid. Optionally also saves them to file.
+
+Args:
+	G:			Generator Object
+	im_class:	Image class as an integer.
+	n_images:	The number of images to generate. Integer.
+	n_columns:	How many columns should be in the final display grid.
+	filename:	If provided, saves the image grid to this file.
+	
+Returns:
+
+"""
 def generate(G, im_class, n_images=1, filename=None, n_columns=2):
-	device = "cuda"
+	# Put into eval mode
 	G.eval()
 	
+	# y is the class vector, z the random noise vectors.
 	y = torch.ones(n_images, dtype=torch.int64, device=device)*im_class
-	z = torch.randn(n_images, G.dim_z, device=device)
+	z = torch.randn(n_images, G.dim_z, device=device) # z is sampled from N(0,1)
 	
+	# Generate images
 	with torch.no_grad():
 		out = G(z, G.shared(y))
 	
-	out = out.cpu()
+	
+	if device == "cuda":
+		out = out.cpu()
+		
 	out = (out + 1) / 2.0 # out is between -1 and 1, we need to shift and scale to 0 to 1
-
+	
+	# Make images into grid
 	out = torchvision.utils.make_grid(out, nrow=n_columns)
+	
+	# Optionally save image
 	if not filename is None:
 		torchvision.utils.save_image(out, filename)
 	
+	# Make numpy for display with pyplot
 	npout = out.numpy()
 	
+	# Display image grid. Grid output is (channels, width, height), but pyplot wants width, height, channels so we have to shuffle dimensions
 	plt.imshow(np.transpose(npout, (1,2,0)), interpolation='nearest')
 	plt.show()
 	
+# CHEESEBURGERS
 if __name__ == "__main__":
-	G = load(os.path.join("..", "BigGAN_model"))
+	G = load(os.path.join("..", "BigGAN_model"), "G_ema.pth")
 	generate(G, 933, n_images=10, n_columns=5)
 	
